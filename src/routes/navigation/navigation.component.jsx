08588d186c21c0a3eff05ee802e4f13f5e1fe70f
wizard <robert@reach52.com>
2023-03-05 19:57:19 +0800

    toggle cart open

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,7 @@
 import { Outlet, Link } from 'react-router-dom'
 import { Fragment, useContext } from 'react'
 import { UserContext } from '../../contexts/user.context'
+import { CartContext } from '../../contexts/cart.context'
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import './navigation.styles.scss'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
@@ -8,7 +9,7 @@ import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 const Navigation = () => {
   const { currentUser } = useContext(UserContext)
-
+  const { isCartOpen } = useContext(CartContext)
   return (
     <Fragment>
       <div className='navigation'>
@@ -30,7 +31,7 @@ const Navigation = () => {
           )}
           <CartIcon />
         </div>
-        <CartDropdown />
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
