10c766f13ab63d36b7a17be6566c4a32a775dd26
wizard <robert@reach52.com>
2023-03-19 19:30:23 +0800

    cart item design

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -9,11 +9,9 @@ const CartDropdown = () => {
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
-        <div className='cart-items'>
-          {cartItems.map((item) => (
-            <CartItem key={item.id} cartItem={item} />
-          ))}
-        </div>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,10 +1,11 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  cartCount: 0,
 })
 
 const addCardItem = (cartItems, productToAdd) => {
@@ -26,11 +27,26 @@ const addCardItem = (cartItems, productToAdd) => {
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
   const [cartItems, setCartItems] = useState([])
+  const [cartCount, setCartCount] = useState(0)
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    )
+    setCartCount(newCartCount)
+  }, [cartItems])
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCardItem(cartItems, productToAdd))
   }
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems }
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+  }
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
