905fc584827e54de249859ef6bf4434a1cffff38
wizard <robert@reach52.com>
2023-04-18 11:58:47 +0800

    redux saga: fetchcategoriesasync thunk to saga

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,13 +3,13 @@ import { useEffect } from 'react'
 import CategoriesPreview from '../categories-preview/categories-preview.component'
 import './shop.styles.scss'
 import Category from '../category/category.component'
-import { fetchCategoriesAsync } from '../../store/categories/category.action'
+import { fetchCategoriesStart } from '../../store/categories/category.action'
 import { useDispatch } from 'react-redux'
 
 const Shop = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(fetchCategoriesAsync())
+    dispatch(fetchCategoriesStart())
   }, [])
   return (
     <Routes>
