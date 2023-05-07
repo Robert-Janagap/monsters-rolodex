ccabb4a8bdcffa6562afba999f396dcad0d3e59c
robert janagap <robert@reach52.com>
2023-05-07 18:16:18 +0800

    typing category action creators

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,13 +1,15 @@
 import { CATEGORIES_ACTION_TYPES } from './category.types'
+import { CategoryAction } from './category.action'
+
 export const CATEGORIES_INITIAL_STATE = {
   categories: [],
   isLoading: false,
-  error: null,
+  error: null
 }
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {}
+  action = {} as CategoryAction
 ) => {
   const { type, payload } = action
   switch (type) {
