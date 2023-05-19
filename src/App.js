1774e5f8dbd466c7e23fcbabf41ef97c37d100d0
robert janagap <robert@reach52.com>
2023-05-19 12:04:56 +0800

    global styles and media queries

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux'
 
 import { checkUserSession } from './store/user/user.action'
 import Spinner from './components/spinner/spinner.component'
+import { GlobalStyle } from './global.styles'
 
 const Home = lazy(() => import('./routes/home/home.component'))
 const Navigation = lazy(() =>
@@ -22,6 +23,7 @@ const App = () => {
   }, [])
   return (
     <Suspense fallback={<Spinner />}>
+      <GlobalStyle />
       <Routes>
         <Route path='/' element={<Navigation />}>
           <Route index element={<Home />} />
