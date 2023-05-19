e0af4664a871523f1e16332a384d0c845caf5fd9
robert janagap <robert@reach52.com>
2023-05-19 12:32:45 +0800

    remaining styles

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,10 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { addItemToCart } from '../../store/cart/cart.action'
 import { selectCartItems } from '../../store/cart/cart.selector'
-
-import './product-card.styles.scss'
+import { CategoryItem } from '../../store/categories/category.types'
+import {ProductCardContainer, Footer, Name, Price}  from './product-card.styles'
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component'
-const ProductCard = ({ product }) => {
+import { FC } from 'react'
+
+type ProductCardProps = {
+  product: CategoryItem
+}
+
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
   const { name, price, imageUrl } = product
   const dispatch = useDispatch()
   const cartItems = useSelector(selectCartItems)
@@ -12,19 +18,19 @@ const ProductCard = ({ product }) => {
   const addProductToCart = () => dispatch(addItemToCart(cartItems, product))
 
   return (
-    <div className='product-card-container'>
+    <ProductCardContainer>
       <img src={imageUrl} alt={`${name}`} />
-      <div className='footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
         <Button
           buttonType={BUTTON_TYPE_CLASSES.inverted}
           onClick={addProductToCart}
         >
           Add to card
         </Button>
-      </div>
-    </div>
+      </Footer>
+    </ProductCardContainer>
   )
 }
 
